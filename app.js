1e73bf821b0609c25045234d862b9087f040a3de
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-04-21 14:20:55 +0700

    fix cors

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var app = express();
 //import
 const mongoose = require('mongoose');
 const products = require('./routes/products');
+const cors = require('cors')
 require('dotenv').config()
 
 mongoose.Promise = global.Promise ;
@@ -28,6 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
